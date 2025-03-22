fc518650eff986d5526ff9100ed2fb2b69632763
MrRichar02 <richarmamadi03@gmail.com>
2025-03-22 13:04:42 -0500

    initial commit for home-manger

---
+++ cli-tools/git.nix
@@ -0,0 +1,10 @@
+{
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      user.email = "richarmamadi03@gmail.com";
+      user.name = "MrRichar02";
+      init.defaultBranch = "main";
+    };
+  };
+}
