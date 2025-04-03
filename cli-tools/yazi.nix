92b3c453ec9957375b0f2ba13bee6fb10a9f7d94
MrRichar02 <richarmamadi03@gmail.com>
2025-04-03 11:00:43 -0500

    adding a keymap for the directory sexto-semestre

--- cli-tools/yazi.nix
+++ cli-tools/yazi.nix
@@ -1,5 +1,10 @@
 {
   programs.yazi = {
     enable = true;
+    keymap = {
+      manager.prepend_keymap = [
+        { run = "cd ~/Documents/sexto-semestre/"; on = ["g s"]; }
+      ];
+    };
   };
 }
