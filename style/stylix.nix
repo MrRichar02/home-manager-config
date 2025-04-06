d7bcd044b6c54eded1e07ef5f58f5e894e72d5ca
MrRichar02 <richarmamadi03@gmail.com>
2025-04-05 21:37:59 -0500

    adding extra config for stylix

--- style/stylix.nix
+++ style/stylix.nix
@@ -4,10 +4,29 @@
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
-    cursor.package = pkgs.bibata-cursors;
-    cursor.name = "Bibata-Modern-Ice";
-    cursor.size = 24;
-  };
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
 
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts.override { fonts = ["JetBrainsMono"];};
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+    };
+
+  };
 
 }
