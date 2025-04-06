9bb01afa835b4ac3a432493db9c2f93ba5d22a03
MrRichar02 <richarmamadi03@gmail.com>
2025-04-05 21:01:23 -0500

    fixing stylix config

--- home.nix
+++ home.nix
@@ -161,8 +161,8 @@
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
-    stylix.cursor.package = pkgs.bibata-cursors;
-    stylix.cursor.name = "Bibata-Modern-Ice";
+    cursor.package = pkgs.bibata-cursors;
+    cursor.name = "Bibata-Modern-Ice";
   };
 
 
