42aa2c7583371f08611f9756a02a25c096bd2438
MrRichar02 <richarmamadi03@gmail.com>
2025-04-11 16:40:29 -0500

    commenting auto login to hyprlando from bash, and taking out some aliases that are unused

--- shells/bash/bash.nix
+++ shells/bash/bash.nix
@@ -1,18 +1,12 @@
 {
   programs.bash = {
     enable = true;
-    shellAliases = {
-      snrsf = "sudo nixos-rebuild switch --flake ~/config-flake";
-      snrtf = "sudo nixos-rebuild test --flake ~/config-flake";
-      nvf = "~/config-flake/editors/nvf/result/bin/nvim";
-      nixv = "~/config-flake/editors/nixvim/result/bin/nvim";
-    };
-    profileExtra = '' 
-      if [[ -z $DISPLAY ]] && [[ $(tty) == /dev/tty1 ]]; then
-        exec Hyprland
-      fi
-
-      '';
+    # profileExtra = '' 
+    #   if [[ -z $DISPLAY ]] && [[ $(tty) == /dev/tty1 ]]; then
+    #     exec Hyprland
+    #   fi
+    #
+    #   '';
   };
 
 }
