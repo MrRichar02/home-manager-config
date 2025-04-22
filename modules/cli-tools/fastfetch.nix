660a580f73be902aac0860f37c563e633759a736
MrRichar02 <richarmamadi03@gmail.com>
2025-04-22 16:03:55 -0500

    fix to toggleble options

--- modules/cli-tools/fastfetch.nix
+++ modules/cli-tools/fastfetch.nix
@@ -2,7 +2,7 @@
 {
   options.myModules.fastfetch.enable = lib.mkEnableOption "enables fastfetch";
 
-  config = lib.mkIf config.fastfetchModule.enable {
+  config = lib.mkIf config.myModules.fastfetch.enable {
     programs.fastfetch = {
       enable = true;
     };
