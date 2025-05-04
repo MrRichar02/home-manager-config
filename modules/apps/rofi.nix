d12b51f19ff91155a15fcff7a6b4169e287169ab
MrRichar02 <richarmamadi03@gmail.com>
2025-05-04 10:06:04 -0500

    refactoring structure

---
+++ modules/apps/rofi.nix
@@ -0,0 +1,16 @@
+{lib, config, pkgs, ...}:
+{
+  options.myModules.rofi.enable = lib.mkEnableOption "enables rofi module";
+
+  config = lib.mkIf config.myModules.rofi.enable {
+
+    programs.rofi = {
+      enable = true;
+      package = pkgs.rofi-wayland;
+      plugins = [
+        pkgs.rofi-emoji-wayland
+      ];
+      theme = "fullscreen-preview";
+    };
+  };
+}
