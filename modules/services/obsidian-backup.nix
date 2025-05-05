1306c4c1f331d76f14f4177254e31219127fc691
MrRichar02 <richarmamadi03@gmail.com>
2025-05-05 18:03:20 -0500

    fix to obsidian service

--- modules/services/obsidian-backup.nix
+++ modules/services/obsidian-backup.nix
@@ -1,36 +1,30 @@
-{lib, config, pkgs, ...}:
+{ pkgs, ... }:
 let
-    gitSyncObsidian = pkgs.writeScriptBin "git-sync-obsidian" ''
-        #!/bin/sh
+  gitSyncObsidian = pkgs.writeScriptBin "git-sync-obsidian" ''
+    #!/bin/sh
 
-        VAULT_DIR="$HOME/Documents/obsidian-notes"
+    VAULT_DIR="$HOME/Documents/obsidian-notes"
     cd $VAULT_DIR || exit 1
-
     git add .
+    git commit -m "$(date '+%Y-%m-%d %H:%M:%S')" || exit 0
+  '';
+in {
+  home.packages = [ gitSyncObsidian ];
 
-        git commit -m "$(date '+%Y-%m-%d %H:%M:%S')" || exit 0
-    '';
-in
-    {
-    options.myServices.obsidian-backup.enable = lib.mkEnableOption "enables obsidian-backup service";
-    config = lib.mkIf config.myServices.obsidian-backup.enable {
-        home.packages = [ gitSyncObsidian ];
-        systemd.user.services.git-sync-obsidian = {
-            Unit = {
-                Description = "Sync Obsidian vault with git";
-                Wants = "git-sync-obsidian.timer";
-            };
-            service = {
-                ExcecStart = "${gitSyncObsidian}/bin/git-sync-obsidian";
-                Type = "Simple";
-            };
-        };
-
-        systemd.user.timers.git-sync-obsidian = {
-            Unit.Description = "Runs Git sync for obsidian vault";
-            Timer.OnCalendar = "*:0/15";
-            Install.WantedBy = [ "timers.target" ];
-        };
+  systemd.user.services.git-sync-obsidian = {
+    Unit = {
+      Description = "Sync Obsidian Vault with GitHub";
+      Wants = "git-sync-obsidian.timer";
     };
-}
+    Service = {
+      ExecStart = "${gitSyncObsidian}/bin/git-sync-obsidian";
+      Type = "simple";
+    };
+  };
 
+  systemd.user.timers.git-sync-obsidian = {
+    Unit.Description = "Run Git Sync for Obsidian Vault";
+    Timer.OnCalendar = "*:0/15";
+    Install.WantedBy = [ "timers.target" ];
+  };
+}
