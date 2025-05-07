c7946eb44a364739a50f66965d5dfbd4f7b04688
MrRichar02 <richarmamadi03@gmail.com>
2025-05-07 14:30:24 -0500

    adding a client package for datbases

--- hosts/laptop-thinkpad/packages.nix
+++ hosts/laptop-thinkpad/packages.nix
@@ -1,54 +1,57 @@
 { pkgs, inputs, ...}:
 {
 
-  nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.allowUnfree = true;
 
-  home.packages = with pkgs; [
+    home.packages = with pkgs; [
 
-    #editors
-    inputs.nixvim-flake.packages.${system}.default
+        #editors
+        inputs.nixvim-flake.packages.${system}.default
 
-    #wallpaper
-    waypaper
+        #wallpaper
+        waypaper
 
-    #chat apps
-    vesktop
-    zapzap
+        #chat apps
+        vesktop
+        zapzap
 
-    #browsers
-    inputs.zen-browser.packages."${system}".default # beta not sigma :(
-    google-chrome
+        #browsers
+        inputs.zen-browser.packages."${system}".default # beta not sigma :(
+        google-chrome
 
-    #system utilities
-    hyprshot
-    hyprlock
-    brightnessctl
-    wl-clipboard
+        #system utilities
+        hyprshot
+        hyprlock
+        brightnessctl
+        wl-clipboard
 
-    #file manager
-    kdePackages.dolphin
+        #file manager
+        kdePackages.dolphin
 
-    #music player
-    spotify
+        #music player
+        spotify
 
-    #ideas
-    jetbrains.idea-ultimate
-    helix
+        #ideas
+        jetbrains.idea-ultimate
+        helix
 
-    #helix lsp
-    nixd
+        #helix lsp
+        nixd
 
-    #comunicaciones
-    wireshark
+        #comunicaciones
+        wireshark
 
-    #note taking
-    obsidian
+        #note taking
+        obsidian
 
-    #hyprland screen sharing dependencies
-    # wireplumber
-    # grim
-    # slurp
-    # kdePackages.xwaylandvideobridge
+        #sql things
+        dbeaver-bin
 
-  ];
+        #hyprland screen sharing dependencies
+        # wireplumber
+        # grim
+        # slurp
+        # kdePackages.xwaylandvideobridge
+
+    ];
 }
