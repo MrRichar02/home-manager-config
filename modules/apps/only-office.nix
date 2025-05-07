e9d56ae560eabed99da9d4d28933d5b083af1567
MrRichar02 <richarmamadi03@gmail.com>
2025-05-07 10:11:51 -0500

    adding settings to onlyoffice

--- modules/apps/only-office.nix
+++ modules/apps/only-office.nix
@@ -6,8 +6,11 @@
         programs.onlyoffice = {
             enable = true;
             settings = {
+                UITheme = "theme-contrast-dark";
                 editorWindowMode = false;
+                forcedRtl = false;
                 maximized = true;
+                titlebar = "custom";
             };
         };
     };
