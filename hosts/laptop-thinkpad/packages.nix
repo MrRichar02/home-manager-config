4755a5be58bc0ac831c17a3a96be24ea830e3fbf
MrRichar02 <richarmamadi03@gmail.com>
2025-05-16 14:05:32 -0500

    adding packages for arqui lab

--- hosts/laptop-thinkpad/packages.nix
+++ hosts/laptop-thinkpad/packages.nix
@@ -1,61 +1,64 @@
 { pkgs, inputs, ...}:
 {
 
-    nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-    home.packages = with pkgs; [
+  home.packages = with pkgs; [
 
-        #editors
-        inputs.nixvim-flake.packages.${system}.default
+    #editors
+    inputs.nixvim-flake.packages.${system}.default
 
-        #wallpaper
-        waypaper
+    #wallpaper
+    waypaper
 
-        #chat apps
-        vesktop
-        zapzap
+    #chat apps
+    vesktop
+    zapzap
 
-        #browsers
-        inputs.zen-browser.packages."${system}".default # beta not sigma :(
-        google-chrome
+    #browsers
+    inputs.zen-browser.packages."${system}".default # beta not sigma :(
+    google-chrome
 
-        #system utilities
-        hyprshot
-        hyprlock
-        brightnessctl
-        wl-clipboard
+    #system utilities
+    hyprshot
+    hyprlock
+    brightnessctl
+    wl-clipboard
 
-        #file manager
-        kdePackages.dolphin
+    #file manager
+    kdePackages.dolphin
 
-        #music player
-        spotify
+    #music player
+    spotify
 
-        #ideas
-        jetbrains.idea-ultimate
-        helix
+    #ideas
+    jetbrains.idea-ultimate
+    helix
 
-        #helix lsp
-        nixd
+    #helix lsp
+    nixd
 
-        #comunicaciones
-        wireshark
+    #comunicaciones
+    wireshark
 
-        #note taking
-        obsidian
+    #note taking
+    obsidian
 
-        #sql things
-        dbeaver-bin
+    #sql things
+    dbeaver-bin
 
-        # #video conferencing
-        # zoom-us
-        # xdg-desktop-portal-hyprland
+    # #video conferencing
+    # zoom-us
+    # xdg-desktop-portal-hyprland
 
-        #hyprland screen sharing dependencies
-        # wireplumber
-        # grim
-        # slurp
-        kdePackages.xwaylandvideobridge
+    #hyprland screen sharing dependencies
+    # wireplumber
+    # grim
+    # slurp
+    kdePackages.xwaylandvideobridge
 
-    ];
+    #lab-2 arqui
+    gitkraken
+    postman
+  ];
 }
