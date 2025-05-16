5e995f43d4847074969c86a8e7dac8679957a334
MrRichar02 <richarmamadi03@gmail.com>
2025-05-16 13:47:10 -0500

    adding a script to qutebrowser for adblocking

--- modules/browsers/qute.nix
+++ modules/browsers/qute.nix
@@ -1,4 +1,4 @@
-{lib, config, ...}:
+{lib, config, pkgs, ...}:
 {
   options.myModules.quteBrowser.enable = lib.mkEnableOption "enables qutebrowser module";
 
@@ -16,6 +16,30 @@
         yt = "https://www.youtube.com/results?search_query={}";
         np = "https://search.nixos.org/packages?channel=unstable&type=packages&query={}";
       };
+
+      greasemonkey = [
+        (pkgs.writeText "yt-ads.js" ''
+  // ==UserScript==
+// @name         Auto Skip YouTube Ads
+// @version      1.1.0
+// @description  Speed up and skip YouTube ads automatically
+// @author       jso8910 and others
+// @match        *://*.youtube.com/*
+// ==/UserScript==
+
+
+document.addEventListener('load', () => {
+    const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button-modern')
+    if (btn) {
+        btn.click()
+    }
+    const ad = [...document.querySelectorAll('.ad-showing')][0];
+    if (ad) {
+        document.querySelector('video').currentTime = 9999999999;
+    }
+}, true);
+        '')
+      ];
     };
   };
 }
