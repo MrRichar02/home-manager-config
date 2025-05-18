d5ab244985d69bbe0d6b6cf5ed06fd03848a42cf
MrRichar02 <richarmamadi03@gmail.com>
2025-05-17 19:13:37 -0500

    changing some conf of qutebrowser

--- modules/browsers/qute.nix
+++ modules/browsers/qute.nix
@@ -5,11 +5,25 @@
   config = lib.mkIf config.myModules.quteBrowser.enable {
     programs.qutebrowser = {
       enable = true;
+
+      extraConfig = ''
+        config.set('colors.webpage.darkmode.enabled', False, 'file://*')
+
+        #privacy
+
+        config.set("content.webgl", False, "*")
+        config.set("content.canvas_reading", False)
+        config.set("content.geolocation", False)
+        config.set("content.webrtc_ip_handling_policy", "default-public-interface-only")
+        config.set("content.cookies.accept", "all")
+        config.set("content.cookies.store", True)
+      '';
       
       settings = {
         colors.webpage.darkmode = {
           enabled = true;
           algorithm = "lightness-cielab";
+          policy.images = "never";
         };
       };
       searchEngines = {
@@ -19,25 +33,25 @@
 
       greasemonkey = [
         (pkgs.writeText "yt-ads.js" ''
-  // ==UserScript==
-// @name         Auto Skip YouTube Ads
-// @version      1.1.0
-// @description  Speed up and skip YouTube ads automatically
-// @author       jso8910 and others
-// @match        *://*.youtube.com/*
-// ==/UserScript==
+          // ==UserScript==
+        // @name         Auto Skip YouTube Ads
+        // @version      1.1.0
+        // @description  Speed up and skip YouTube ads automatically
+        // @author       jso8910 and others
+        // @match        *://*.youtube.com/*
+        // ==/UserScript==
 
 
-document.addEventListener('load', () => {
-    const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button-modern')
-    if (btn) {
-        btn.click()
-    }
-    const ad = [...document.querySelectorAll('.ad-showing')][0];
-    if (ad) {
-        document.querySelector('video').currentTime = 9999999999;
-    }
-}, true);
+        document.addEventListener('load', () => {
+            const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button-modern')
+            if (btn) {
+                btn.click()
+            }
+            const ad = [...document.querySelectorAll('.ad-showing')][0];
+            if (ad) {
+                document.querySelector('video').currentTime = 9999999999;
+            }
+        }, true);
         '')
       ];
     };
