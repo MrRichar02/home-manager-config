69d3a4ddc95e313669fb7a3a47eadb30627099dc
MrRichar02 <richarmamadi03@gmail.com>
2025-05-24 10:52:35 -0500

    changing config for mpv

--- modules/video/mpv.nix
+++ modules/video/mpv.nix
@@ -1,10 +1,17 @@
-{lib, config, ...}:
+{lib, config, pkgs, ...}:
 {
   options.myModules.mpv.enable = lib.mkEnableOption "enables mpv module";
 
   config = lib.mkIf config.myModules.mpv.enable {
     programs.mpv = {
       enable = true;
+      package = pkgs.mpv-unwrapped.wrapper { mpv =  pkgs.mpv-unwrapped.override { vapoursynthSupport = true; }; youtubeSuport = true; };
+      config = {
+        profile = "gpu-hq";
+        force-window = true;
+        ytdl-format = "bestvideo+bestaudio";
+        cache-default = 4000000;
+      };
     };
   };
 }
