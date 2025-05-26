944270f657f6d0259d0e8f9af6f482d4c8f35be1
MrRichar02 <richarmamadi03@gmail.com>
2025-05-26 10:00:36 -0500

    disabling youtube add blocker

--- modules/browsers/qute.nix
+++ modules/browsers/qute.nix
@@ -56,29 +56,29 @@
         modelos1 = "https://udea.zoom.us/j/91785418283";
       };
  
-      greasemonkey = [
-        (pkgs.writeText "yt-ads.js" ''
-          // ==UserScript==
-        // @name         Auto Skip YouTube Ads
-        // @version      1.1.0
-        // @description  Speed up and skip YouTube ads automatically
-        // @author       jso8910 and others
-        // @match        *://*.youtube.com/*
-        // ==/UserScript==
-
-
-        document.addEventListener('load', () => {
-            const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button-modern')
-            if (btn) {
-                btn.click()
-            }
-            const ad = [...document.querySelectorAll('.ad-showing')][0];
-            if (ad) {
-                document.querySelector('video').currentTime = 9999999999;
-            }
-        }, true);
-        '')
-      ];
+      # greasemonkey = [
+      #   (pkgs.writeText "yt-ads.js" ''
+      #     // ==UserScript==
+      #   // @name         Auto Skip YouTube Ads
+      #   // @version      1.1.0
+      #   // @description  Speed up and skip YouTube ads automatically
+      #   // @author       jso8910 and others
+      #   // @match        *://*.youtube.com/*
+      #   // ==/UserScript==
+      #
+      #
+      #   document.addEventListener('load', () => {
+      #       const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button-modern')
+      #       if (btn) {
+      #           btn.click()
+      #       }
+      #       const ad = [...document.querySelectorAll('.ad-showing')][0];
+      #       if (ad) {
+      #           document.querySelector('video').currentTime = 9999999999;
+      #       }
+      #   }, true);
+      #   '')
+      # ];
     };
   };
 }
