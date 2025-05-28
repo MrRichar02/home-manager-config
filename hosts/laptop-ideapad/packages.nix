9d72e438a946649b7128ff839020e9485ff11ce9
MrRichar02 <richarmamadi03@gmail.com>
2025-05-27 21:45:14 -0500

    adding a new host for the ideapad laptop

---
+++ hosts/laptop-ideapad/packages.nix
@@ -0,0 +1,62 @@
+{ pkgs, inputs, ...}:
+{
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    #ciscoPacketTracer8
+
+    #editors
+    inputs.nvf-flake.packages.${system}.default
+
+    #password manager
+    bitwarden-desktop
+
+    #wallpaper
+    waypaper
+
+    #chat apps
+    vesktop
+    zapzap
+
+    #browsers
+    inputs.zen-browser.packages."${system}".default # beta not sigma :(
+    #google-chrome
+
+    #system utilities
+    hyprshot
+    hyprlock
+    brightnessctl
+    wl-clipboard
+
+    # video
+    ffmpeg_6-full
+
+
+    #file manager
+    kdePackages.dolphin
+
+    #music player
+    #spotify
+    youtube-music
+
+    #ideas
+    jetbrains.idea-ultimate
+
+    #note taking
+    obsidian
+
+    #sql things
+    dbeaver-bin
+
+
+    #hyprland screen sharing dependencies
+    # wireplumber
+    # grim
+    # slurp
+    kdePackages.xwaylandvideobridge
+
+    #lab-2 arqui
+    postman
+  ];
+}
