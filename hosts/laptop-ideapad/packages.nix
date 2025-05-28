61f2cef31a078ad4893f84799f7b5fb093d1e45c
MrRichar02 <richarmamadi03@gmail.com>
2025-05-28 14:28:39 -0500

    uncomenting cisco packet tracer for ideapad

--- hosts/laptop-ideapad/packages.nix
+++ hosts/laptop-ideapad/packages.nix
@@ -4,7 +4,7 @@
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
-    #ciscoPacketTracer8
+    ciscoPacketTracer8
 
     #editors
     inputs.nvf-flake.packages.${system}.default
