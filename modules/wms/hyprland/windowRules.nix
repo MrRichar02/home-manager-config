c334beb70708ee7db9ce14ed8082eaab556c5e69
MrRichar02 <richarmamadi03@gmail.com>
2025-06-08 19:01:47 -0500

    changes to style and adding a windows rule for youtube music

--- modules/wms/hyprland/windowRules.nix
+++ modules/wms/hyprland/windowRules.nix
@@ -8,6 +8,7 @@
         "workspace 5 silent, class:ZapZap"
         "workspace 8 silent, class:obsidian"
         "workspace 10, class:spotify"
+        "workspace 10, class:com.github.th_ch.youtube_music"
       ];
     };
   };
