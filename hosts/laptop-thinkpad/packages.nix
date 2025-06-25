9efd2c1a15bab783296604a7014aa9f6907e3fc1
MrRichar02 <richarmamadi03@gmail.com>
2025-06-24 21:30:24 -0500

    removing unecesary packages

--- hosts/laptop-thinkpad/packages.nix
+++ hosts/laptop-thinkpad/packages.nix
@@ -4,11 +4,6 @@
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
-    #ciscoPacketTracer8
-
-    # minikube
-    # kubernetes-helm
-
     #editors
     inputs.nvf-flake.packages.${system}.default
 
@@ -24,7 +19,6 @@
 
     #browsers
     inputs.zen-browser.packages."${system}".default # beta not sigma :(
-    #google-chrome
 
     #system utilities
     hyprshot
@@ -35,29 +29,17 @@
     # video
     ffmpeg_6-full
 
-
     #file manager
     kdePackages.dolphin
 
     #music player
-    #spotify
     youtube-music
-
-    #ideas
-    jetbrains.idea-ultimate
 
     #note taking
     obsidian
 
     #sql things
     dbeaver-bin
-
-
-    #hyprland screen sharing dependencies
-    # wireplumber
-    # grim
-    # slurp
-    kdePackages.xwaylandvideobridge
 
     #lab-2 arqui
     postman
