8438116fdae7952f24cd8922923a9c4f07311174
MrRichar02 <richarmamadi03@gmail.com>
2025-08-23 09:16:47 -0500

    adding some config to obsidian

--- modules/apps/obsidian.nix
+++ modules/apps/obsidian.nix
@@ -12,6 +12,7 @@
         vimMode = true;
         showLineNumber = true;
         attachmentFolderPath = "./attachments";
+				readableLineLength = false;
       };
       vaults."Documents/obsidian-notes".enable = true;
     };
